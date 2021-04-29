5773ab56b49a248748b461d6dea6f524fe4af890
Matthew Jones <mejones92@gmail.com>
2021-04-29 21:47:52 +0100

    Updated mongoose connection string and added static path for public folder.

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
+const path = require('path');
 const PORT = process.env.PORT || 3000;
 
 const app = express();
@@ -12,10 +13,12 @@ app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname + '/public')));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
   useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
   useFindAndModify: false
 });
 
