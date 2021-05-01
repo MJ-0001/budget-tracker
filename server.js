8eb27e3856dbe526799ca50e7bb0d43bfc4a20b0
Matthew Jones <mejones92@gmail.com>
2021-05-01 22:00:20 +0100

    Updated server.js config.

--- server.js
+++ server.js
@@ -17,6 +17,8 @@ app.use(express.static(path.join(__dirname + '/public')));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
   useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
   useFindAndModify: false
 });
 
